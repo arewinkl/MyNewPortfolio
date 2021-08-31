aa18d4be48fba01c6fcf6add438672148d131dff
A Joseph <alecrewinkel@gmail.com>
2021-08-31 13:48:14 -0600

    paths

--- src/components/main.js
+++ src/components/main.js
@@ -10,7 +10,7 @@ import Projects from './projects';
 
 const Main = () => (
     <Switch>
-        <Route  exact path ="/reactPortfolio" component={LandingPage} />
+        <Route  exact path ="/MyNewPortfolio" component={LandingPage} />
         <Route  path ="/aboutme" component={AboutMe} />
         <Route  path ="/contact" component={Contact} />
         <Route  path ="/projects" component={Projects} />
